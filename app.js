205d31119be0508c94f054d1bfda95d4b82e2cd7
Sara Filekovic <sara.filekovic@outlook.com>
2021-11-24 23:03:12 +0100

    Added testimonials slider

--- app.js
+++ app.js
@@ -11,5 +11,39 @@ closeMenu.addEventListener('click', () => {
   navbar.style.top = '-100%';
 });
 
+const prev = document.getElementById('prevButton');
+const next = document.getElementById('nextButton');
+const testimonial = document.getElementsByClassName('testimonial');
+
+let testimonialSlideIndex = 1;
+testimonialSlideshow(testimonialSlideIndex);
+
+function changeSlides(slideInd) {
+  testimonialSlideshow(testimonialSlideIndex += slideInd);
+}
+
+function testimonialSlideshow(index) {
+  let i;
+  if(index > testimonial.length) {
+    testimonialSlideIndex = 1;
+  }
+  if(index < 1) {
+    testimonialSlideIndex = testimonial.length;
+  }
+  for (i = 0; i < testimonial.length; i++) {
+    testimonial[i].style.display = "none";
+  }
+  testimonial[testimonialSlideIndex-1].style.display = "block";
+}
+
+next.addEventListener('click', () => {
+  changeSlides(1);
+});
+prev.addEventListener('click', () => {
+  changeSlides(-1);
+});
+
+
+
 
 
